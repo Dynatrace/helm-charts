a16d6974f8c56f842c0088aa6f674c6baa2bcbf5
Michael Rynkiewicz <michael.rynkiewicz@dynatrace.com>
2020-11-30 11:16:52 +0100

    modified linting script

--- testing/lint.sh
+++ testing/lint.sh
@@ -5,8 +5,15 @@ for file in "$(pwd)"/*/; do
   cd "$file/" || exit 5
   if [ -f "Chart.yml" ] || [ -f "Chart.yaml" ]; then
     echo "Linting $file"
-    helm template --debug --set oneagent.apiUrl="test-url" . || exit 10
-    helm template --debug --set oneagent.apiUrl="test-uel" . | yamllint --strict - || exit 15
+
+    if ! helm template --debug --set oneagent.apiUrl="test-url" .; then
+      echo "could not parse template. something is wrong with template files of directory '$file'"
+      exit 10
+    fi
+
+    if ! helm template --debug --set oneagent.apiUrl="test-url" . | yamllint --strict -; then
+       echo "linter returned with error. check yaml formatting in files of directory '$file'." && exit 15
+    fi
   else
     echo "$file does not contain Chart file"
   fi
