a554761a02ebf21afad856552d648b78b2074b05
Michael Rynkiewicz <michael.rynkiewicz@dynatrace.com>
2020-11-30 12:17:19 +0100

    fixed linter errors

--- testing/lint.sh
+++ testing/lint.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-for file in "$(pwd)"/*/; do
+for directory in "$(pwd)"/*/; do
 
-  cd "$file/" || exit 5
+  cd "$directory/" || exit 5
   if [ -f "Chart.yml" ] || [ -f "Chart.yaml" ]; then
-    echo "Linting $file"
+    echo "Linting $directory"
 
     if ! helm template --debug --set oneagent.apiUrl="test-url" .; then
-      echo "could not parse template. something is wrong with template files of directory '$file'"
+      echo "could not parse template. something is wrong with template files of directory '$directory'"
       exit 10
     fi
 
-    if ! helm template --debug --set oneagent.apiUrl="test-url" . | yamllint -d "{extends: default, rules: {line-length: disable}}" --strict -; then
-       echo "linter returned with error. check yaml formatting in files of directory '$file'." && exit 15
+    if ! helm template --debug --set oneagent.apiUrl="test-url" . | yamllint -d "{extends: default, rules: {line-length: disable, trailing-spaces: disable}}" --strict -; then
+       echo "linter returned with error. check yaml formatting in files of directory '$directory'." && exit 15
     fi
   else
-    echo "$file does not contain Chart file"
+    echo "$directory does not contain Chart file. skipping..."
   fi
 done
