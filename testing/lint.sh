0d635b8aa05824d5fc93a341cf6f50c870924cc7
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2021-03-11 10:49:42 +0100

    migrate oao chart to dto (#81)

--- testing/lint.sh
+++ testing/lint.sh
@@ -6,12 +6,12 @@ for directory in "$(pwd)"/*/; do
   if [ -f "Chart.yml" ] || [ -f "Chart.yaml" ]; then
     echo "Linting $directory"
 
-    if ! helm template --debug --set oneagent.apiUrl="test-url" .; then
+    if ! helm template --debug --set oneagent.apiUrl="test-url" --set apiUrl="test-url" --set secret.apiToken="test-token" secret.paasToken="test-token" .; then
       echo "could not parse template. something is wrong with template files of directory '$directory'"
       exit 10
     fi
 
-    if ! helm template --debug --set oneagent.apiUrl="test-url" . | yamllint -d "{extends: default, rules: {line-length: disable, trailing-spaces: disable}}" --strict -; then
+    if ! helm template --debug --set oneagent.apiUrl="test-url"  --set secret.apiToken="test-token" secret.paasToken="test-token" . | yamllint -d "{extends: default, rules: {line-length: disable, trailing-spaces: disable}}" --strict -; then
        echo "linter returned with error. check yaml formatting in files of directory '$directory'." && exit 15
     fi
   else
