a8c8ef9310164323170691592ac7750bb35cb5f4
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2021-08-26 09:26:36 +0200

    Implements gabriel's changes

--- testing/lint.sh
+++ testing/lint.sh
@@ -7,12 +7,12 @@ for directory in "$(pwd)"/*/; do
     if [ -f "Chart.yml" ] || [ -f "Chart.yaml" ]; then
       echo "Linting $directory"
 
-      if ! helm template --debug --set oneagent.apiUrl="test-url" --set apiUrl="test-url" --set secret.apiToken="test-token" secret.paasToken="test-token" .; then
+      if ! helm template --debug --set oneagent.apiUrl="test-url",apiUrl="test-url",secret.apiToken="test-token",secret.paasToken="test-token",apiToken="test-token",paasToken="test-token" .; then
         echo "could not parse template. something is wrong with template files of directory '$directory'"
         exit 10
       fi
 
-      if ! helm lint --debug --set oneagent.apiUrl="test-url",secret.apiToken="test-token",secret.paasToken="test-token" . ; then
+      if ! helm lint --debug --set oneagent.apiUrl="test-url",secret.apiToken="test-token",secret.paasToken="test-token",apiToken="test-token",paasToken="test-token" . ; then
         echo "linter returned with error. check yaml formatting in files of directory '$directory'." && exit 15
       fi
     else
